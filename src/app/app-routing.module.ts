e9851bfe6dd52910ddd590d3b226003fafd4fbf9
Bharathipk <95739346+Bharathipk@users.noreply.github.com>
2021-12-08 20:27:03 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { NewsComponent } from './components/news/news.component';
 
 
 const routes: Routes = [
-  { path:'', redirectTo: '/home' },
+  { path:'', redirectTo: '/home', pathMatch: 'full' },
   { path:'home', component:HomeComponent },
 
   { path:'world', component:WorldComponent },
